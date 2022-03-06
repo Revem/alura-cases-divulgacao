d131e5beda9a29aa7397a082f9f14d83e9bee44d
Revem <revemvi@hotmail.com>
2022-03-06 02:16:30 -0300

    finalizando curso

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/perguntas',
+        destination: '/faq',
+        permanent: true,
+      },
+    ]
+  },
 }
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -3,6 +3,7 @@ import Footer from '../../components/patterns/Footer'
 import Link from '../../components/Link'
 import { theme } from '../../theme/theme';
 import { Image, Box, Text, Icon, Input, Button } from '../../theme/components';
+import PageTitle from '../../components/PageTitle';
 
 const LOGO_ALURA_URL = 'images/alura-logo.svg';
 const SIDE_IMAGE_URL = 'images/alura-cases.png'
@@ -97,9 +98,7 @@ function SideImage() {
 export default function HomeScreen() {
   return (
     <Box>
-      <Head>
-        <title>Home - Alura Cases Campanha</title>
-      </Head>
+      <PageTitle>Home - Alura Cases Campanha</PageTitle>
       <Box
         as="main"
         styleSheet={{
